83f09fcee264b1a993c73b35f8b32deb4f00428c
Sara Girard <sarag.bsa@gmail.com>
2026-01-01 20:21:01 -0500

    setting up base spring backend

---
+++ ngd/src/main/java/com/saragirard/ngd/NgdApplication.java
@@ -0,0 +1,13 @@
+package com.saragirard.ngd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgdApplication.class, args);
+	}
+
+}
---
+++ ngd/src/test/java/com/saragirard/ngd/NgdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saragirard.ngd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
