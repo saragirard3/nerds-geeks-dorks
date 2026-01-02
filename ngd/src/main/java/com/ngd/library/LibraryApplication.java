28f8d1dd8c4b2c0e7fc3af16d8052a7ca58868d0
Sara Girard <sarag.bsa@gmail.com>
2026-01-01 20:27:04 -0500

    fix errors in spring

--- ngd/src/main/java/com/ngd/library/LibraryApplication.java
+++ ngd/src/main/java/com/ngd/library/LibraryApplication.java
@@ -1,13 +1,13 @@
-package com.saragirard.ngd;
+package com.ngd.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NgdApplication {
+public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NgdApplication.class, args);
+		SpringApplication.run(LibraryApplication.class, args);
 	}
 
 }
