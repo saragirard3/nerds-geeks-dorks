fbd910eb4bfea7758c05958ce92d7078da410e29
Sara Girard <sarag.bsa@gmail.com>
2026-01-24 11:01:50 -0500

    change is coming

--- library/src/main/java/com/ngd/library/card/Cards.java
+++ library/src/main/java/com/ngd/library/card/Cards.java
@@ -1,5 +1,10 @@
 package com.ngd.library.card;
 
-public class Cards {
+import java.io.Serializable;
+
+public class Cards implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     
+
 }
