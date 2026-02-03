5d4acc6485a25bc2f333b3dc82e4b23799a43346
Sara Girard <sara@paditim.com>
2026-02-02 21:08:10 -0500

    update

--- magic/src/main/java/com/ngd/magic/card/Card.java
+++ magic/src/main/java/com/ngd/magic/card/Card.java
@@ -2,23 +2,19 @@ package com.ngd.magic.card;
 
 import java.io.Serializable;
 
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// @Table
-// @NoArgsConstructor
-// @AllArgsConstructor
-// @Getter
-// @Setter
-// @ToString
-// @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Card implements Serializable {
     private static final long serialVersionUID = 1L;
 
     // @Id
     // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // private Long id;
-    // private String name;    
+    private Long id;
+    private String name;    
 
 }
